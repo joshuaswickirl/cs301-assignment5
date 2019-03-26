66b511a0aff9fde409aa0dcd595e68bf09336dfc
Joshua Swick <joshuaswickirl@gmail.com>
2019-03-26 17:07:52 -0600

    Add insertion sort function's logic and related unit tests

--- sorting_lists.py
+++ sorting_lists.py
@@ -4,8 +4,27 @@
 
 def insertion_sort(ulist):
     """
+    Returns sorted numerical list using an insertion sort algorithm
     """
-    pass
+    length = len(ulist)
+    sorted_list = []
+    sorted_list.insert(0,ulist[0])
+    for item in ulist[1:]:
+        index = 0
+        index_found = False
+        if item < sorted_list[index]:
+            sorted_list.insert(index,item)
+            index_found = True
+        while not index_found:
+            if index == len(sorted_list)-1:
+                sorted_list.append(item)
+                index_found = True
+            elif item > sorted_list[index] and item < sorted_list[index+1]:
+                sorted_list.insert(index+1,item)
+                index_found = True
+            else:
+                index += 1
+    return sorted_list
 
 
 def bubble_sort(ulist):
--- test_sorting_lists.py
+++ test_sorting_lists.py
@@ -6,13 +6,16 @@ import pytest
 from sorting_lists import insertion_sort, bubble_sort, selection_sort, merge_sort
 
 def test_insertion_sort_1():
-    pass
+    ulist = [3,2,1]
+    assert sort_list(ulist) == [1,2,3]
 
 def test_insertion_sort_2():
-    pass
+    ulist = [1,4,5,9,2,15,0]
+    assert sort_list(ulist) == [0,1,2,4,5,9,15]
 
 def test_insertion_sort_3():
-    pass
+    ulist = [21,9,-1,4]
+    assert sort_list(ulist) == [-1,4,9,21]
 
 
 def test_bubble_sort_1():
