5427a72fac957c903d18f274ce9a329360373490
Edgar301 <46509883+Edgar301@users.noreply.github.com>
2019-03-28 15:51:52 -0600

    Update sorting_lists.py

--- sorting_lists.py
+++ sorting_lists.py
@@ -13,11 +13,19 @@ def bubble_sort(ulist):
     """
     pass
 
-
 def selection_sort(ulist):
-    """
-    """
-    pass
+   ***
+   Selections Sort 
+   ***
+   for slot in range(len(ulist)-1,0,-1):
+       posOfMax = 0
+       for location in range(1,slot+1):
+           if ulist[location]> ulist[posOfMax]:
+                posOfMax = location
+
+       sort = ulist[slot]
+       ulist[slot] = ulist[posOfMax]
+       ulist[posOfMax] = sort
 
 
 def merge_sort(ulist):
