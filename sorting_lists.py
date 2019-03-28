e080a2073c3d133dd9022a37dece05544a7fbaa2
dkpanikkar <48968556+dkpanikkar@users.noreply.github.com>
2019-03-28 15:50:55 -0600

    Update sorting_lists.py

--- sorting_lists.py
+++ sorting_lists.py
@@ -10,8 +10,14 @@ def insertion_sort(ulist):
 
 def bubble_sort(ulist):
     """
+    returns sorted list using bubble sort method
     """
-    pass
+    for passnum in range(len(ulist)-1,0,-1): #gives a value of position
+        for i in range(passnum):#it searches for the position equal to i
+            if ulist[i]>ulist[i+1]: #if the value of position i is larger than
+                temp = ulist[i]
+                ulist[i] = ulist[i+1]
+                ulist[i+1] = temp
 
 
 def selection_sort(ulist):
