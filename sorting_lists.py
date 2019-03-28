42e597803f43ea65dc0567a8771bda64e275e130
Edgar301 <46509883+Edgar301@users.noreply.github.com>
2019-03-27 19:12:42 -0600

    Update sorting_lists.py

--- sorting_lists.py
+++ sorting_lists.py
@@ -15,8 +15,20 @@ def bubble_sort(ulist):
 
 
 def selection_sort(ulist):
-    """
-    """
+    for slot in range(len(ulist)-1,0,-1):
+        posOfMax = 0
+        for location in range(1,slot+1):
+            if ulist[location] > ulist[posOfMAx]:
+                posOfMax = location
+                
+    sort = ulist[slot]
+    ulist[slot] = ulist[posOfMax]
+    ulist[posOfMax] = sort
+
+ulist = [13, 3, 64, 12, 34, 27, 59, 84, 68, 37, 76, 16, 21, 42, 39]
+selection_sort(ulist)
+print(ulist)
+   
     pass
 
 
