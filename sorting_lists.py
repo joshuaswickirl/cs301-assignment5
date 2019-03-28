ece81a30c0a426f3b5fc2a948e7792aad37d5dab
Joshua Swick <joshuaswickirl@gmail.com>
2019-03-28 15:57:33 -0600

    Add return ulist and fixed whitespace

--- sorting_lists.py
+++ sorting_lists.py
@@ -14,18 +14,19 @@ def bubble_sort(ulist):
     pass
 
 def selection_sort(ulist):
-   ***
-   Selection-Sort 
-   ***
-   for slot in range(len(ulist)-1,0,-1):
-       posOfMax = 0
-       for location in range(1,slot+1):
-           if ulist[location]> ulist[posOfMax]:
+    """
+    Selection-Sort 
+    """
+    for slot in range(len(ulist)-1,0,-1):
+        posOfMax = 0
+        for location in range(1,slot+1):
+            if ulist[location]> ulist[posOfMax]:
                 posOfMax = location
 
-       sort = ulist[slot]
-       ulist[slot] = ulist[posOfMax]
-       ulist[posOfMax] = sort
+        sort = ulist[slot]
+        ulist[slot] = ulist[posOfMax]
+        ulist[posOfMax] = sort
+    return ulist
 
 
 def merge_sort(ulist):
