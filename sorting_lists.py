1f1a5523a6ee70e6da7c67c82e2f5ab0807c8228
Joshua Swick <joshuaswickirl@gmail.com>
2019-03-29 14:34:14 -0600

    Add merge sort functions and tests

--- sorting_lists.py
+++ sorting_lists.py
@@ -22,5 +22,39 @@ def selection_sort(ulist):
 
 def merge_sort(ulist):
     """
+    Recurisvely splits list into individual items
+    then uses merge to join them in order.
     """
-    pass
+    if len(ulist) <= 1:
+        return ulist
+    else:
+        mid = len(ulist) // 2
+        list1 = ulist[:mid]
+        list2 = ulist[mid:]
+        return merge_sorted_lists(merge_sort(list1), merge_sort(list2))
+
+
+def merge_sorted_lists(list1,list2=[]):
+    """
+    Returns two sorted lists combined into one
+    sorted list.
+    """
+    combined_list = []
+    items_left = True
+    while items_left:
+        len_list1 = len(list1)
+        len_list2 = len(list2)
+        if len_list1 == 0 and len_list2 == 0:
+            items_left = False
+        elif len_list1 == 0 and len_list2 > 0:
+            combined_list = combined_list + list2
+            items_left = False
+        elif len_list2 == 0 and len_list1 > 0:
+            combined_list = combined_list + list1
+            items_left = False
+        elif list1[0] >= list2[0]:
+            combined_list.append(list2.pop(0))
+        else:
+            combined_list.append(list1.pop(0))
+
+    return combined_list
--- test_sorting_lists.py
+++ test_sorting_lists.py
@@ -3,7 +3,7 @@
 #
 
 import pytest
-from sorting_lists import insertion_sort, bubble_sort, selection_sort, merge_sort
+from sorting_lists import insertion_sort, bubble_sort, selection_sort, merge_sort, merge_sorted_lists
 
 def test_insertion_sort_1():
     pass
@@ -37,12 +37,23 @@ def test_selection_sort_3():
 
 def test_merge_sort_1():
     ulist = [3,2,1]
-    assert selection_sort(ulist) == [1,2,3]
+    assert merge_sort(ulist) == [1,2,3]
 
 def test_merge_sort_2():
     ulist = [1,4,5,9,2,15,0]
-    assert selection_sort(ulist) == [0,1,2,4,5,9,15]
+    assert merge_sort(ulist) == [0,1,2,4,5,9,15]
 
 def test_merge_sort_3():
     ulist = [21,9,-1,4]
-    assert selection_sort(ulist) == [-1,4,9,21]
+    assert merge_sort(ulist) == [-1,4,9,21]
+
+
+def test_merge_sorted_lists_1():
+    list1 = [1,3,5]
+    list2 = [0,2,4,6]
+    assert merge_sorted_lists(list1,list2) == [0,1,2,3,4,5,6]
+
+def test_merge_sorted_lists_2():
+    list1 = [5,9,10]
+    list2 = [0,2,4,6,8,9]
+    assert merge_sorted_lists(list1,list2) == [0,2,4,5,6,8,9,9,10]
