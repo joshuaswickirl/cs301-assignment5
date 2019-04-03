7a1e546b2d13ec439f9e00e367d1a5fcef3a357b
Joshua Swick <joshuaswickirl@gmail.com>
2019-04-03 09:53:49 -0600

    Add description of bubble sort run time

--- sorting_lists.py
+++ sorting_lists.py
@@ -7,13 +7,23 @@ def insertion_sort(ulist):
     """
     pass
 
+#
+#   Bubble Sort
+#
+#   Bubble sort runs in n**2 time. This is because
+#   for an item in the index, the item may have to be
+#   compared to every othe item in the list. This process
+#   then has to happen for each item in the list. This is
+#   a nested for loop.
+#   
 
 def bubble_sort(ulist):
     """
-    returns sorted list using bubble sort method
+    Returns sorted list using bubble sort method
     runs at O(n^2)
     """
     for passnum in range(len(ulist)-1,0,-1): #gives a value of position
+        print(ulist)
         for i in range(passnum):#it searches for the position equal to i
             if ulist[i]>ulist[i+1]: #if the value of position i is larger than
                 temp = ulist[i]
