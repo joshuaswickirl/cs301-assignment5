7428ecfae53ec9eea07b065e82b3c0439e1d99df
Joshua Swick <joshuaswickirl@gmail.com>
2019-04-03 09:59:03 -0600

    Add description of selection sort runtime

--- sorting_lists.py
+++ sorting_lists.py
@@ -13,12 +13,22 @@ def bubble_sort(ulist):
     """
     pass
 
+#
+#   Selection Sort
+#
+#   Select sort runs in n**2 time because the
+#   algorithm searchs for the largest item in 
+#   the list, n number of times. This is a nested
+#   for loop.
+
 def selection_sort(ulist):
     """
-    Selection-Sort: A function used to sort a list by initializing the largest number in the first slot 
+    Selection-Sort: A function used to sort a list by 
+    initializing the largest number in the first slot 
     until it works its way down the entire list.
     
-    Run time: My prediction for the Big-O runtime is O(n^2) because of the for-loop and nested for-loop that calculates "n."
+    Run time: My prediction for the Big-O runtime is O(n^2) 
+    because of the for-loop and nested for-loop that calculates "n."
     """
     for slot in range(len(ulist)-1,0,-1):
         posOfMax = 0
